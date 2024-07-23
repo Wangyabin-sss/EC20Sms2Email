a3934b698487bb45b06f908983fecc14fc645fc9
Wangyabin-sss <W15217681799@163.com>
2024-07-23 17:17:46 +0800

    完善run.sh脚本

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	arm-linux-gnueabihf-gcc -o smsmail main.c SMS.c utf.c uart.c log.c -I./ -lpthread
+	/home/ubuntu/Desktop/rkcpu/rk3566-tspi/buildroot/output/rockchip_rk3566/host/bin/aarch64-linux-gcc -o smsmail main.c SMS.c utf.c uart.c log.c -I./ -lpthread
--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
 cd /root
-export EMAILKEYVAL=email授权码
+export TZ=CST-8
+export EMAILKEYVAL=email密钥
+ttydev="$(echo `ls /dev/ttyUSB*` | awk '{print $3}')"
+
 while true
 do
-./smsmail /dev/ttyUSB2
-sleep 5
+	if [ `ps -ef | grep smsmail | grep -v "grep" | wc -l` == "0" ]
+	then
+		./smsmail $ttydev &
+	fi
+
+	if [ "$ttydev" != "$(echo `ls /dev/ttyUSB*` | awk '{print $3}')" ]
+    then
+        ttydev="$(echo `ls /dev/ttyUSB*` | awk '{print $3}')"
+		killall -9 smsmail
+    fi
+
+	sleep 10
 done
 
